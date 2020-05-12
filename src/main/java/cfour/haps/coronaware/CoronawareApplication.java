c246f7998a9add8b1ec341425f2185fbc16453e8
alifherdin <alifherdin@gmail.com>
2020-05-12 23:33:07 +0700

    commit

--- src/main/java/cfour/haps/coronaware/CoronawareApplication.java
+++ src/main/java/cfour/haps/coronaware/CoronawareApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoronawareApplication {
+public class CoronAwareApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoronawareApplication.class, args);
+		SpringApplication.run(CoronAwareApplication.class, args);
 	}
 
 }
