49db3851925998dde0193f6ce19c3af9c658d907
alifherdin <alifherdin@gmail.com>
2020-05-12 23:37:54 +0700

    first commit

---
+++ src/main/java/cfour/haps/coronaware/CoronAwareApplication.java
@@ -0,0 +1,13 @@
+package cfour.haps.coronaware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronAwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronAwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/c04/haps/coronaware/CoronawareApplicationTests.java
@@ -0,0 +1,13 @@
+package c04.haps.coronaware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronawareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
